d8fbf377c48553d90e474567688d9c467cb709c5
YadavEshnithin <eshnithinyadav@gmail.com>
2025-05-22 17:50:22 +0530

    conversation

--- shipping.sh
+++ shipping.sh
@@ -1,4 +1,117 @@
+# #!/bin/bash
+# START_TIME=$(date +%s)
+# USERID=$(id -u)
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+# LOGS_FOLDER="/var/log/roboshop-logs"
+# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+# LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+# SCRIPT_DIR=$PWD
+
+# mkdir -p $LOGS_FOLDER
+# echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# if [ $USERID -ne 0 ]
+# then
+#     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+#     exit 1 #give other than 0 upto 127
+# else
+#     echo "You are running with root access" | tee -a $LOG_FILE
+# fi
+
+# echo "please enter password"
+# read -s "MYSQL_ROOT_PASSWORD"
+
+# VALIDATE(){
+#     if [ $1 -eq 0 ]
+#     then
+#         echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+#     else
+#         echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+#         exit 1
+#     fi
+# }
+
+
+
+# dnf install maven -y &>>$LOG_FILE
+# VALIDATE $? "installing mysql" 
+
+# id roboshop &>>$LOG_FILE
+
+# if [ $? -ne 0 ]
+# then
+#     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+# else
+#     echo "user already created"
+# fi
+
+# mkdir -p /app
+# VALIDATE $? "creating app directory shipping" 
+
+# curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
+# VALIDATE $? "downloading shipping" 
+
+# rm -rf /app/*
+# cd /app 
+# unzip /tmp/shipping.zip &>>$LOG_FILE
+# VALIDATE $? "unzipping shipping" 
+
+
+# cd /app 
+# mvn clean package &>>$LOG_FILE
+# VALIDATE $? "Packaging the shipping application" 
+
+# mv target/shipping-1.0.jar shipping.jar 
+# VALIDATE $? "renaming shipping" 
+
+# cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
+
+# systemctl daemon-reload
+# systemctl enable shipping 
+# systemctl start shipping
+# VALIDATE $? "starting shipping" 
+
+
+# dnf install mysql -y
+# VALIDATE $? "installing mysql for shipping" 
+
+
+# mysql -h mysql.rshopdaws84s.site -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
+# if [ $? -ne 0 ]
+# then
+#     mysql -h mysql.rshopdaws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
+#     mysql -h mysql.rshopdaws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
+#     mysql -h mysql.rshopdaws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
+#     VALIDATE $? "Loading data into MySQL"
+# else
+#     echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
+# fi 
+
+
+
+# systemctl restart shipping &>>$LOG_FILE
+# VALIDATE $? "Restart shipping"
+
+# END_TIME=$(date +%s)
+# TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+# echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+
+
+
+
+
+
+
+
+
+
+
 #!/bin/bash
+
 START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
@@ -13,6 +126,7 @@ SCRIPT_DIR=$PWD
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
+# check the user has root priveleges or not
 if [ $USERID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
@@ -21,9 +135,10 @@ else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-echo "please enter password"
-read -s "MYSQL_ROOT_PASSWORD"
+echo "Please enter root password to setup"
+read -s MYSQL_ROOT_PASSWORD
 
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
@@ -34,50 +149,48 @@ VALIDATE(){
     fi
 }
 
-
-
 dnf install maven -y &>>$LOG_FILE
-VALIDATE $? "installing mysql" 
+VALIDATE $? "Installing Maven and Java"
 
 id roboshop &>>$LOG_FILE
-
 if [ $? -ne 0 ]
 then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating roboshop system user"
 else
-    echo "user already created"
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
 fi
 
-mkdir -p /app
-VALIDATE $? "creating app directory shipping" 
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
 
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
-VALIDATE $? "downloading shipping" 
+curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading shipping"
 
 rm -rf /app/*
 cd /app 
 unzip /tmp/shipping.zip &>>$LOG_FILE
-VALIDATE $? "unzipping shipping" 
+VALIDATE $? "unzipping shipping"
 
+mvn clean package  &>>$LOG_FILE
+VALIDATE $? "Packaging the shipping application"
 
-cd /app 
-mvn clean package &>>$LOG_FILE
-VALIDATE $? "Packaging the shipping application" 
-
-mv target/shipping-1.0.jar shipping.jar 
-VALIDATE $? "renaming shipping" 
+mv target/shipping-1.0.jar shipping.jar  &>>$LOG_FILE
+VALIDATE $? "Moving and renaming Jar file"
 
 cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
 
-systemctl daemon-reload
-systemctl enable shipping 
-systemctl start shipping
-VALIDATE $? "starting shipping" 
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Daemon Realod"
 
+systemctl enable shipping  &>>$LOG_FILE
+VALIDATE $? "Enabling Shipping"
 
-dnf install mysql -y
-VALIDATE $? "installing mysql for shipping" 
+systemctl start shipping &>>$LOG_FILE
+VALIDATE $? "Starting Shipping"
 
+dnf install mysql -y  &>>$LOG_FILE
+VALIDATE $? "Install MySQL"
 
 mysql -h mysql.rshopdaws84s.site -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]
@@ -88,9 +201,7 @@ then
     VALIDATE $? "Loading data into MySQL"
 else
     echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
-fi 
-
-
+fi
 
 systemctl restart shipping &>>$LOG_FILE
 VALIDATE $? "Restart shipping"
@@ -99,14 +210,3 @@ END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
 echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
-
-
-
-
-
-
-
-
-
-
-
